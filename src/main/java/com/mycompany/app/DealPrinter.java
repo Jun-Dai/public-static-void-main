8123478d8bcd40bbc10ca4ab17c1164c03d98410
Paul.Hall <paul.hall@hotmail.co.uk>
2013-05-16 18:12:32 +0100

    Fixed deal printer so that the price is to 2 decimal places and right aligned.

--- src/main/java/com/mycompany/app/DealPrinter.java
+++ src/main/java/com/mycompany/app/DealPrinter.java
@@ -8,7 +8,7 @@ public class DealPrinter {
 
         List<Deals.SimpleHotel> hotels = deals.getHotels();
         for (Deals.SimpleHotel hotel : hotels) {
-            sb.append(String.format("%30.30s : $%s\n", hotel.name, hotel.price));
+            sb.append(String.format("%30.30s : $%8.2f\n", hotel.name, hotel.price));
         }
 
         return sb.toString();
